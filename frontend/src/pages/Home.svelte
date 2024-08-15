6c5973e253338a97cc3c1ab15b102c2ca02f29a3
Yann Le Vagueres <yann.lv@live.fr>
2024-08-15 14:57:44 +0200

    Renamed files

--- frontend/src/pages/Home.svelte
+++ frontend/src/pages/Home.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import Canvas from '../lib/Canvas.svelte';
-    import Logo from '../lib/Logo.svelte';
+    import Overlay from '../lib/Overlay.svelte';
     import ColorPicker from '../lib/ColorPicker.svelte';
 </script>
-<meta name=viewport content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<Logo />
+
+<Overlay />
 <ColorPicker></ColorPicker>
 <Canvas />
