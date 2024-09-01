40884c0debd67b78ee0e3d833945778f429116db
Yann Le Vagueres <yann.lv@live.fr>
2024-09-01 10:43:47 +0200

    Deleted go backend

--- backend-go/model/CanvasModel.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Canvas struct {
-	Valid  bool
-	Colors []uint8
-}
--- backend-go/model/ConnectionModel.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "github.com/gorilla/websocket"
-
-// Connection represents an active connection and all data associated with it
-type Connection struct {
-	WebSockerConn     *websocket.Conn
-	SubscribedChannel chan PixelColorUpdatePubSubMessage
-}
